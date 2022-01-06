f77665008a426216f44cf633d752894e2ae2b185
LucasJS331 <69942512+LucasJS331@users.noreply.github.com>
2022-01-06 19:01:31 -0300

    updade configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategoriasModule } from './categorias/categorias.module';
 import { JogadoresModule } from './jogadores/jogadores.module';
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule,ConfigService } from '@nestjs/config'
 
+const config_service = new ConfigService();
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://admin_sr:kdWcpu1jWvUSlU6Y@clustermogodb-79l5n.mongodb.net/sradmbackend?retryWrites=true&w=majority',
+    MongooseModule.forRoot(config_service.get("MONGO_URI"),
     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }),
     CategoriasModule,
     JogadoresModule,
